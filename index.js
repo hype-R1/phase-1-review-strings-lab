2a5a47d1337a3ccc9125d75e9bdc992039f4b919
Michael <mjpj@live.com>
2021-11-01 11:57:45 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const currentUser = 'Mike Pace'
+
+// const welcomeMessage = 'Welcome to Flatbook, '
+
+// const welcomeMessage = "Welcome to Flatbook, "+ currentUser +"!";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
